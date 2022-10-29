3211aa200ea8ce397cb5d422144e49d11e84d392
Nikolay Ulanov <iamnickolasulanov@gmail.com>
2022-10-30 00:08:40 +0500

    new file:   .eslintrc.yml modified:   bin/brain-games.js modified:   makefile modified:   package-lock.json modified:   package.json modified:   src/cli.js

--- src/cli.js
+++ src/cli.js
@@ -1,8 +1,8 @@
-import readline from "readline-sync";
+import readline from 'readline-sync';
 
-export const greetings = () => {
-    const greeting = 'Welcome to the Brain Games!';
-    console.log(greeting);
-    const userName = readline.question('May I have your name? ');
-    console.log(`Hello, ${userName}!`);
+export default () => {
+  const greeting = 'Welcome to the Brain Games!';
+  console.log(greeting);
+  const userName = readline.question('May I have your name? ');
+  console.log(`Hello, ${userName}!`);
 };
